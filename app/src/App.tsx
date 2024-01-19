b7621912fdfd079ff8f3f2a6c589790c9787274d
Giorgio <tran.giorgio@gmail.com>
2024-01-19 11:40:27 -1000

    Create basic plots for Mesonet and Sage Node data

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,35 +1,136 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Chart.js Line Chart",
+    },
+  },
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [sageData, setSageData] = useState<any>(null);
+  const [mesonetData, setMesonetData] = useState<any>(null);
 
+  async function getMesonetData() {
+    const res = await fetch(
+      `https://api.synopticdata.com/v2/stations/timeseries?&stid=004HI&units=metric,speed|kph,pres|mb&recent=1440&24hsummary=1&qc_remove_data=off&qc_flags=on&qc_checks=all&hfmetars=1&showemptystations=1&precip=1&token=${
+        import.meta.env.VITE_MESONET_PUBLIC_TOKEN
+      }`
+    );
+    console.log("mesonet token", import.meta.env.VITE_MESONET_PUBLIC_TOKEN);
+    const data = await res.json();
+    console.log("mesonet data", data);
+    setMesonetData(data);
+  }
+
+  async function getSageNodeData() {
+    const res = await fetch("https://data.sagecontinuum.org/api/v1/query", {
+      method: "POST",
+      // headers: {
+      //   "Content-Type": "application/json",
+      // },
+      body: JSON.stringify({
+        start: "-1h",
+        tail: 10,
+        filter: {
+          name: "env.temperature",
+          vsn: "W097",
+        },
+      }),
+    });
+    const data = await res.text();
+    console.log("text data", data);
+    const parsedData = data.split("\n").map((line) => {
+      // console.log("line", line);
+      if (line !== "") {
+        return JSON.parse(line);
+      }
+    });
+    console.log("parsed data", parsedData);
+    setSageData(parsedData.filter((data) => data !== undefined));
+  }
+
+  useEffect(() => {
+    getMesonetData();
+    getSageNodeData();
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {mesonetData && (
+        <Line
+          options={options}
+          data={{
+            labels: mesonetData?.STATION[0].OBSERVATIONS?.date_time?.map(
+              (date: string) => {
+                return new Date(date).toLocaleTimeString();
+              }
+            ),
+            datasets: [
+              {
+                label: "Mesonet",
+                data: mesonetData?.STATION[0].OBSERVATIONS?.air_temp_set_1?.map(
+                  (temp: number) => {
+                    return temp;
+                  }
+                ),
+                borderColor: "rgb(255, 99, 132)",
+                backgroundColor: "rgba(255, 99, 132, 0.5)",
+              },
+            ],
+          }}
+        />
+      )}
+      {
+        sageData && (
+          <Line
+            options={options}
+            data={{
+              labels: sageData?.map((data: any) => {
+                return new Date(data.timestamp).toLocaleTimeString();
+              }),
+              datasets: [
+                {
+                  label: "Sage Node",
+                  data: sageData?.map((data: any) => {
+                    return data.value;
+                  }),
+                  borderColor: "rgb(99, 255, 132)",
+                  backgroundColor: "rgba(99, 255, 132, 0.5)",
+                },
+              ],
+            }}
+          />
+        )
+      }
+    </div>
+  );
 }
 
-export default App
+export default App;
